151c49a762b0b0d3ac54ed6bce115a6c74b0c65e
tylerstout <tyler.j.stout.15@dartmouth.edu>
2014-01-30 11:31:23 -0800

    some changes, still needs improvement

--- TipCalculator/thetipViewController.m
+++ TipCalculator/thetipViewController.m
@@ -9,9 +9,33 @@
 #import "thetipViewController.h"
 
 @interface thetipViewController ()
-
 @end
 
 @implementation thetipViewController
+- (IBAction)Price:(UITextField *)sender {
+}
+
+- (IBAction)Tip:(UITextField *)sender {
+}
+
+- (IBAction)calculate:(id)sender {
+    
+    float p = [Price.text floatValue];
+    float t = [Tip.text floatValue];
+    float amount = (p * t)/100;
+    
+    NSString *msg = [NSString stringWithFormat:@"Leave a tip of $%.2f", amount];
+   
+    UIAlertView *alert = [[[UIAlertView alloc]
+                           initWithTitle:@"Tip"
+                           message:msg
+                           delegate: self
+                           cancelButtonTitle: @"OK"
+                           otherButtonTitles: nil];
+                          
+                          [alert show];
+                         
+                          
+}
 
 @end
